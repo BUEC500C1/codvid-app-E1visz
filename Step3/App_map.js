4771f901dfeaad4f3cf6828c0f0a7e689c74834a
E1visz <55214592+E1visz@users.noreply.github.com>
2020-04-23 12:06:15 -0400

    Update App_map.js

--- Step3/App_map.js
+++ Step3/App_map.js
@@ -1,8 +1,9 @@
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
-import React from 'react';
-import {StyleSheet, Text, View, TextInput, Platform} from 'react-native';
+import * as React from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default
+
 class App extends React.Component {
   render() {
     return (
@@ -11,11 +12,14 @@ class App extends React.Component {
         provider={PROVIDER_GOOGLE}
         showsUserLocation
         initialRegion={{
+        // Latitude and longitude are used to tell the position of the object
+        // BU address 
         latitude: 42.3505,
         longitude: -71.1054,
-        latitudeDelta: 0.1,
-        longitudeDelta: 0.1,
-        }}
+        // latitudeDelta and longitudeDelta are used to 
+        // provide the zoom options in the map.
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421}}
       />
       
     );
